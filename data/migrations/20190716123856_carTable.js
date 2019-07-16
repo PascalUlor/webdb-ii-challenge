ffb5ff105d7b12e8b8c3f9e5c660e69e18a82df1
PascalUlor <pascalulor@yahoo.com>
2019-07-16 13:49:08 +0100

    initial commit create database create migrations

---
+++ data/migrations/20190716123856_carTable.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("cardb", table => {
+    table.increments();
+    table.string("make").notNullable();
+    table.string("model").notNullable();
+    table
+      .string("VIN")
+      .unique()
+      .notNullable();
+    table.string("mileage").notNullable();
+    table.string("transmission").defaultTo("unknown");
+    table.string("status").defaultTo("unknown");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTabeleIfExists("cardb");
+};
